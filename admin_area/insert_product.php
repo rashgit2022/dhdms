bcfcdfefc3caebc3f5faef02c2509b07d16056d5
Rashmi Wathsala <rashmisandamini1999@gmail.com>
2024-02-04 09:38:07 +0530

    Inserting products

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -1,3 +1,55 @@
+<?php
+include('../includes/connect.php');
+if(isset($_POST['insert_product'])){
+    
+    $product_title=$_POST['product_title'];
+    $description=$_POST['description'];
+    $product_keywords=$_POST['product_keywords'];
+    $product_category=$_POST['product_category'];
+    $product_price=$_POST['product_price'];
+    $product_status='true';
+
+    //accessing images
+    $product_image1=$_FILES['product_image1']['name'];
+    $product_image2=$_FILES['product_image2']['name'];
+    $product_image3=$_FILES['product_image3']['name'];
+
+    //accessing image tmp name
+    $temp_image1=$_FILES['product_image1']['tmp_name'];
+    $temp_image2=$_FILES['product_image2']['tmp_name'];
+    $temp_image3=$_FILES['product_image3']['tmp_name'];
+
+    //checking Empty condition
+    if($product_title=='' or 
+    $description=='' or 
+    $product_keywords=='' or 
+    $product_category=='' or 
+    $product_price=='' or 
+    $product_image1=='' or 
+    $product_image2=='' or 
+    $product_image3==''){
+        echo "<script>alert('Please fill all the available fields')</script>";
+        exit();
+    }else{
+        move_uploaded_file($temp_image1,"./product_images/$product_image1");
+        move_uploaded_file($temp_image2,"./product_images/$product_image2");
+        move_uploaded_file($temp_image3,"./product_images/$product_image3");
+
+        //insert query
+        $insert_products="insert into `products` (product_title,product_description,
+        product_keywords,category_id,product_image1,product_image2,product_image3,
+        product_price,date,status) values ('$product_title','$description',
+        '$product_keywords','$product_category','$product_image1','$product_image2',
+        '$product_image3','$product_price',NOW(),'$product_status')";
+        $result_query=mysqli_query($con,$insert_products);
+        if($result_query){
+            echo "<script>alert('Successfully inserted the products')</script>";
+        }
+    }
+
+    
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,8 +94,16 @@
             <div class="form-outline mb-4 w-50 m-auto ">
                 <select name="product_category" id="" class="form-select">
                     <option value="">Select a category</option>
-                    <option value="">Vegetables</option>
-                    <option value="">Fruits</option>
+                    <?php
+                        $select_query="Select * from categories";
+                        $result_query=mysqli_query($con,$select_query);
+                        while($row=mysqli_fetch_assoc($result_query)){
+                            $category_title=$row['category_title'];
+                            $category_id=$row['category_id'];
+                            echo "<option value='$category_id'>$category_title</option>";
+                        }
+                    ?>
+                    
                 </select>  
             </div>&nbsp; 
 
